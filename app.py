2bca42712873952ed175ce50c94d3bae24190f91
Johan MarinE <jdmarinb@gmail.com>
2022-01-15 09:59:57 -0500

    Se mejoro la compatibilidad con apis y body

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
-from flask import Flask, jsonify, send_from_directory
+import json
 import API.functions as f_api
-
+from pydantic import BaseModel
+from flask import Flask, jsonify, send_from_directory, request
 
 app = Flask(__name__)
 
@@ -12,16 +13,22 @@ app.config["CLIENT_CSV"] = os.getcwd() + '/files/'
 def test():  
     return jsonify({"Bienvenido":"La API esta en linea"})
 
+@app.route('/prueba', methods=['GET'])
+def get_prueba():
+    df = f_api.get_data('SELECT TOP(10) * FROM dbo.creCreditos;')
+    return jsonify(df)
+
 @app.route('/query', methods=['GET'])
 def get_query():
-    df = f_api.get_data('SELECT TOP(10) * FROM dbo.creCreditos;')
+    data = json.loads(request.data)
+    df = f_api.get_data(data['sql_query'])
     return jsonify(df)
 
 @app.route('/csv', methods=['GET'])
 def get_csv():
-    sql_query = 'SELECT TOP(10) * FROM dbo.creCreditos;' # TODO: Esta linea se remmplaza por la entrada
+    data = json.loads(request.data)
     path = app.config["CLIENT_CSV"] + 'data.csv'
-    resp = f_api.get_file(sql_query, path)
+    resp = f_api.get_file(data['sql_query'], path)
     if 'Error' in resp:
         return jsonify(resp)
     else:
--- main.py
+++ main.py
@@ -1,25 +1,41 @@
 import os
 from fastapi import FastAPI
+from pydantic import BaseModel
+from datetime import datetime
 import API.functions as f_api
 from starlette.responses import FileResponse
 
+# Query model
+class Query(BaseModel):
+    source: str
+    sql_query: str
+    date: datetime = datetime.now()
+    
 app = FastAPI()
 
+# Root route
 @app.get('/')
 def read_root():
     return {"Bienvenido":"La API esta en linea"}
 
-@app.get('/query')
-def get_query():
-    sql_query = 'SELECT TOP(10) * FROM dbo.creCreditos;' # TODO: Esta linea se remmplaza por la entrada
+# Get query like json
+@app.get('/prueba')
+def get_query(query: Query):
+    sql_query = 'SELECT TOP(10) * FROM dbo.creCreditos;' # Query de prueba
     df = f_api.get_data(sql_query)
     return df
 
+# Get query like json
+@app.get('/query')
+def get_query(query: Query):
+    df = f_api.get_data(query.sql_query)
+    return df
+
+# Get query like csv
 @app.get('/csv')
-def get_csv():
-    sql_query = 'SELECT TOP(10) * FROM dbo.creCreditos;' # TODO: Esta linea se remmplaza por la entrada
+def get_csv(query: Query):
     file_path = os.getcwd() + '/files/data.csv'
-    resp = f_api.get_file(sql_query, file_path)
+    resp = f_api.get_file(query.sql_query, file_path)
     if 'Error' in resp:
         return resp
     else:
